dfbf9a86458971259f787fdb0a17294668c1eabf
YonahPy <jrjonas487@gmail.com>
2023-08-09 12:37:01 -0300

    menu suspenso feito

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ function menu() {
                 menuBar.appendChild(ul);
                 ul.style.display = 'block';
             });
+            
         } else {
             logoBotao.removeChild(menuBar);
             menuAberto = false;
@@ -41,3 +42,60 @@ function menu() {
 }
 
 menu();
+
+function menuSuspenso(){
+    const features = document.getElementById('features');
+    const company = document.getElementById('company');
+
+    const menuFeatures = document.createElement('div');
+    menuFeatures.classList.add('menu-features');
+    menuFeatures.innerHTML = '<li><img src="intro-section-with-dropdown-navigation-main/images/icon-todo.svg" alt="icon icon-todo"> Todo List</li> <li> <img src="intro-section-with-dropdown-navigation-main/images/icon-calendar.svg" alt="icon-calendar"> Calendar</li> <li><img src="intro-section-with-dropdown-navigation-main/images/icon-reminders.svg" alt="icon-reminders">Reminders</li> <li> <img src="intro-section-with-dropdown-navigation-main/images/icon-planning.svg" alt="icon-planning"> Planning</li>';
+
+
+    const menuCompany = document.createElement('div');
+    menuCompany.classList.add('menu-company');
+    menuCompany.innerHTML = '<li>History</li> <li>Our Team</li> <li>Blog</li>';
+
+    let arrowUp1 = document.querySelector('#arrow-up1');
+    let arrowUp2 = document.querySelector('#arrow-up2');
+    let arrowDown1 = document.querySelector('#arrow-down1');
+    let arrowDown2 = document.querySelector('#arrow-down2');
+
+   
+    let alternarFeatures = false;
+    
+    features.addEventListener('click', function(){
+        if (!alternarFeatures){
+            features.appendChild(menuFeatures);
+            alternarFeatures = true;
+            arrowDown1.style.display = 'none';
+            arrowUp1.style.display = 'inline'
+
+
+        } else{
+            features.removeChild(menuFeatures);
+            alternarFeatures = false;
+            arrowDown1.style.display = 'inline';
+            arrowUp1.style.display = 'none';
+            
+        }
+    })
+
+    let alternarCompany = false;
+
+    company.addEventListener('click', function(){
+        if (!alternarCompany){
+            company.appendChild(menuCompany);
+            alternarCompany = true;
+            arrowDown2.style.display = 'none';
+            arrowUp2.style.display = 'inline'
+        } else{
+            company.removeChild(menuCompany);
+            alternarCompany = false;
+            arrowDown2.style.display = 'inline';
+            arrowUp2.style.display = 'none';
+        }
+    })
+
+}
+menuSuspenso()
